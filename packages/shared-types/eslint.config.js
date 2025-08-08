896be3514042834e1607a98112ecbe727394521d
Saif Latifi <saif.latifi@gmail.com>
2025-08-08 20:00:11 +0100

    Chore: Run global lint

--- packages/shared-types/eslint.config.js
+++ packages/shared-types/eslint.config.js
@@ -2,5 +2,5 @@ import { eslintBaseConfig } from '@repo/eslint-config';
 import ts from 'typescript-eslint';
 
 export default ts.config(...eslintBaseConfig({ isWeb: false }), {
-	ignores: ['eslint.config.js', '.prettierrc.js', "**/dist/**"]
+	ignores: ['eslint.config.js', '.prettierrc.js', '**/dist/**']
 });
