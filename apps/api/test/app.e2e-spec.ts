4aaf7887f7f47149ddb78dfb0ffcecfa780a02a7
Saif Latifi <saif.latifi@gmail.com>
2025-08-20 10:04:58 +0100

    fix: update reference in mock provider

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -3,10 +3,11 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { DatabaseService } from '@/database/database.service';
 
 class MockDatabaseService {
 	async onModuleInit() {
-		// Mock database connection logic
+		// no-op for mock
 	}
 }
 
@@ -17,8 +18,8 @@ describe('AppController (e2e)', () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
 			imports: [AppModule]
 		})
-			.overrideProvider('DatabaseService')
-			.useClass(MockDatabaseService)
+			.overrideProvider(DatabaseService)
+			.useValue(new MockDatabaseService())
 			.compile();
 
 		app = moduleFixture.createNestApplication();
